d51af876c0a77156bc60bf06f8586a667e1da525
camcgreen <c.c.green@outlook.com>
2023-12-16 20:37:19 +0000

    Add ability to change background colour

--- app/components/create/Scene.tsx
+++ app/components/create/Scene.tsx
@@ -1,12 +1,24 @@
 'use client'
 import { useEffect } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import { CameraControls, Environment } from '@react-three/drei'
 import { PhoneModel } from '@/app/components/models/phone'
 import { TabletModel } from '@/app/components/models/tablet'
 import { LaptopModel } from '@/app/components/models/laptop'
 
-function Scene({ device }: { device: 'iPhone' | 'iPad' | 'MacBook' }) {
+type SceneProps = {
+  device: 'iPhone' | 'iPad' | 'MacBook'
+}
+
+function Scene({ device }: SceneProps) {
+  const { gl } = useThree()
+  const form = document.getElementById('toolbar') as HTMLFormElement
+  useFrame(() => {
+    const color = (
+      form.elements.namedItem('backgroundColor') as HTMLInputElement
+    ).value
+    gl.setClearColor(color)
+  })
   switch (device) {
     case 'iPhone':
       return <PhoneModel scale={[0.1, 0.1, 0.1]} />
@@ -17,8 +29,7 @@ function Scene({ device }: { device: 'iPhone' | 'iPad' | 'MacBook' }) {
   }
 }
 
-function SceneEditor({ device }: { device: 'iPhone' | 'iPad' | 'MacBook' }) {
-  useEffect(() => {}, [])
+function SceneEditor({ device }: SceneProps) {
   return (
     <div className='w-full md:w-9/12 rounded-xl h-screen-middle-sm lg:h-screen-middle bg-dark-sky'>
       <Canvas camera={{ position: [0, 0, 7], fov: 50 }}>
