084abba6d630af64856b46b247991552e6f59209
camcgreen <c.c.green@outlook.com>
2023-12-16 12:27:44 +0000

    Fix build error

--- app/components/dashboard/SceneList.tsx
+++ app/components/dashboard/SceneList.tsx
@@ -14,8 +14,8 @@ export default async function SceneList({
     <ul className='grid gap-4 grid-cols-2 lg:grid-cols-3 auto-rows-fr '>
       <ButtonNew userId={userId} />
       {scenes.map((scene: SceneType) => (
-        <Link href={`/create/${scene.id}`}>
-          <li key={scene.id} className='cursor-pointer'>
+        <Link href={`/create/${scene.id}`} key={scene.id}>
+          <li className='cursor-pointer'>
             <img
               src='/images/splash-min.jpg'
               className='rounded-xl'
